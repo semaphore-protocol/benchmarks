25f4be8272ebc8756155b939565c6f8dd1d1eb16
cedoor <me@cedoor.dev>
2023-05-05 10:56:18 +0100

    feat: add button to verify proof

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Group } from "@semaphore-protocol/group"
 import { Identity } from "@semaphore-protocol/identity"
-import { generateProof as _generateProof } from "@semaphore-protocol/proof"
-import { useState } from "react"
+import { FullProof, generateProof as _generateProof, verifyProof as _verifyProof } from "@semaphore-protocol/proof"
+import { useCallback, useState } from "react"
 import "./App.css"
 import semaphoreLogo from "./assets/semaphore.svg"
 
 function App() {
     const [time, setTime] = useState<number>(0.0)
     const [info, setInfo] = useState<string>("Click on the button to generate a Semaphore proof.")
+    const [proof, setProof] = useState<FullProof>()
 
-    async function generateProof() {
+    const generateProof = useCallback(async function () {
         const identity = new Identity()
         const group = new Group(1)
 
@@ -28,8 +29,26 @@ function App() {
         setTime((t1 - t0) / 1000)
         setInfo("Proof generated!")
 
+        setProof(proof)
         console.log(proof)
-    }
+    }, [])
+
+    const verifyProof = useCallback(
+        async function () {
+            if (proof) {
+                setInfo("Verifiying proof...")
+                const t0 = performance.now()
+
+                await _verifyProof(proof, 20)
+
+                const t1 = performance.now()
+
+                setTime((t1 - t0) / 1000)
+                setInfo("Proof verified!")
+            }
+        },
+        [proof]
+    )
 
     return (
         <div className="App">
@@ -39,6 +58,9 @@ function App() {
             <h1>Semaphore</h1>
             <h3>{time} s</h3>
             <button onClick={() => generateProof()}>Generate proof</button>
+            <br />
+            <br />
+            <button onClick={() => verifyProof()}>Verify proof</button>
             <p className="info">{info}</p>
         </div>
     )
