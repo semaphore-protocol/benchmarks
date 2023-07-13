29a3958d3db4b81aba69cb49e5c3d3eebb81cf4c
vplasencia <v.pcalana@gmail.com>
2023-07-13 09:11:06 +0200

    style: format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,149 +1,135 @@
-import {
-  Box,
-  Button,
-  Flex,
-  List,
-  ListIcon,
-  ListItem,
-  Text,
-} from "@chakra-ui/react";
-import { Group } from "@semaphore-protocol/group";
-import { Identity } from "@semaphore-protocol/identity";
-import { generateProof, verifyProof } from "@semaphore-protocol/proof";
-import { useCallback, useState } from "react";
-import { MdCheckCircle } from "react-icons/md";
-import GroupMembers from "./components/GroupMembers";
-import Navbar from "./components/Navbar";
-import TreeDepth from "./components/TreeDepth";
+import { Box, Button, Flex, List, ListIcon, ListItem, Text } from "@chakra-ui/react"
+import { Group } from "@semaphore-protocol/group"
+import { Identity } from "@semaphore-protocol/identity"
+import { generateProof, verifyProof } from "@semaphore-protocol/proof"
+import { useCallback, useState } from "react"
+import { MdCheckCircle } from "react-icons/md"
+import GroupMembers from "./components/GroupMembers"
+import Navbar from "./components/Navbar"
+import TreeDepth from "./components/TreeDepth"
 
 const functions = [
-  "new Identity",
-  "new Group",
-  "generateProof",
-  "verifyProof",
-  "add Member to Group",
-  "update Group Member",
-];
+    "new Identity",
+    "new Group",
+    "generateProof",
+    "verifyProof",
+    "add Member to Group",
+    "update Group Member"
+]
 
 function App() {
-  const [treeDepth, setTreeDepth] = useState<number>(20);
-  const [groupMembers, setGroupMembers] = useState<number>(100);
-  const [times, setTimes] = useState<number[]>([]);
+    const [treeDepth, setTreeDepth] = useState<number>(20)
+    const [groupMembers, setGroupMembers] = useState<number>(100)
+    const [times, setTimes] = useState<number[]>([])
 
-  const runFunctions = useCallback(
-    async function () {
-      const times = [];
+    const runFunctions = useCallback(
+        async function () {
+            const times = []
 
-      const [identity, time0] = await run(() => {
-        return new Identity();
-      });
+            const [identity, time0] = await run(() => {
+                return new Identity()
+            })
 
-      times.push(time0);
+            times.push(time0)
 
-      setTimes(times);
+            setTimes(times)
 
-      const [group, time1] = await run(() => {
-        const members = Array.from(Array(groupMembers - 1).keys());
+            const [group, time1] = await run(() => {
+                const members = Array.from(Array(groupMembers - 1).keys())
 
-        console.log(members.length);
+                console.log(members.length)
 
-        return new Group(1, treeDepth, [...members, identity.commitment]);
-      });
+                return new Group(1, treeDepth, [...members, identity.commitment])
+            })
 
-      times.push(time1);
+            times.push(time1)
 
-      setTimes(times.slice());
+            setTimes(times.slice())
 
-      const [proof, time2] = await run(async () => {
-        return await generateProof(identity, group, 1, 1);
-      });
+            const [proof, time2] = await run(async () => {
+                return await generateProof(identity, group, 1, 1)
+            })
 
-      times.push(time2);
+            times.push(time2)
 
-      setTimes(times.slice());
+            setTimes(times.slice())
 
-      const [, time3] = await run(async () => {
-        return await verifyProof(proof, treeDepth);
-      });
+            const [, time3] = await run(async () => {
+                return await verifyProof(proof, treeDepth)
+            })
 
-      times.push(time3);
+            times.push(time3)
 
-      setTimes(times.slice());
+            setTimes(times.slice())
 
-      const [, time4] = await run(() => {
-        const member = new Identity().commitment;
+            const [, time4] = await run(() => {
+                const member = new Identity().commitment
 
-        console.log(group.members.length);
+                console.log(group.members.length)
 
-        group.addMember(member);
+                group.addMember(member)
 
-        console.log(group.members.length);
-      });
-      times.push(time4);
+                console.log(group.members.length)
+            })
+            times.push(time4)
 
-      setTimes(times.slice());
+            setTimes(times.slice())
 
-      const [, time5] = await run(() => {
-        const member = new Identity().commitment;
+            const [, time5] = await run(() => {
+                const member = new Identity().commitment
 
-        console.log(group.members[0]);
+                console.log(group.members[0])
 
-        group.updateMember(0, member);
+                group.updateMember(0, member)
 
-        console.log(group.members[0]);
-      });
-      times.push(time5);
+                console.log(group.members[0])
+            })
+            times.push(time5)
 
-      setTimes(times.slice());
-    },
-    [treeDepth, groupMembers]
-  );
+            setTimes(times.slice())
+        },
+        [treeDepth, groupMembers]
+    )
 
-  async function run(callback: () => any): Promise<[any, number]> {
-    const t0 = performance.now();
+    async function run(callback: () => any): Promise<[any, number]> {
+        const t0 = performance.now()
 
-    const result = await callback();
+        const result = await callback()
 
-    const t1 = performance.now();
+        const t1 = performance.now()
 
-    return [result, t1 - t0];
-  }
+        return [result, t1 - t0]
+    }
 
-  return (
-    <Flex flexDir="column" flex="1">
-      <Navbar />
-      <Flex flex="1" align="center" justify="center">
-        <Flex flexDir="column" gap={4} width="400px">
-          <TreeDepth value={treeDepth} onChange={setTreeDepth} />
-          <GroupMembers
-            value={groupMembers}
-            onChange={setGroupMembers}
-            max={2 ** treeDepth}
-          />
+    return (
+        <Flex flexDir="column" flex="1">
+            <Navbar />
+            <Flex flex="1" align="center" justify="center">
+                <Flex flexDir="column" gap={4} width="400px">
+                    <TreeDepth value={treeDepth} onChange={setTreeDepth} />
+                    <GroupMembers value={groupMembers} onChange={setGroupMembers} max={2 ** treeDepth} />
 
-          <Button onClick={() => runFunctions()} size="sm" my="3">
-            Run functions
-          </Button>
+                    <Button onClick={() => runFunctions()} size="sm" my="3">
+                        Run functions
+                    </Button>
 
-          <List spacing={3}>
-            {functions.map((f, i) => (
-              <ListItem key={i}>
-                <Flex justify={"space-between"}>
-                  <Box>
-                    {times[i] && (
-                      <ListIcon as={MdCheckCircle} color="green.500" />
-                    )}
-                    <b>{f}</b>
-                  </Box>
-                  <Text>{times[i] ? times[i] : 0} ms</Text>
+                    <List spacing={3}>
+                        {functions.map((f, i) => (
+                            <ListItem key={i}>
+                                <Flex justify={"space-between"}>
+                                    <Box>
+                                        {times[i] && <ListIcon as={MdCheckCircle} color="green.500" />}
+                                        <b>{f}</b>
+                                    </Box>
+                                    <Text>{times[i] ? times[i] : 0} ms</Text>
+                                </Flex>
+                            </ListItem>
+                        ))}
+                    </List>
                 </Flex>
-              </ListItem>
-            ))}
-          </List>
+            </Flex>
         </Flex>
-      </Flex>
-    </Flex>
-  );
+    )
 }
 
-export default App;
+export default App
