4a563ea761714c61503ff15604cce242d1d0e27a
cedoor <me@cedoor.dev>
2023-09-01 12:22:40 +0200

    chore: update pse domain

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa"
 export default function Navbar() {
     return (
         <HStack width="100%" justify="space-between" p="6">
-            <Link href="https://semaphore.appliedzkp.org/" isExternal>
+            <Link href="https://semaphore.pse.dev/" isExternal>
                 <Image src="semaphore-logo.svg" htmlWidth={150} />
             </Link>
             <Link href="https://github.com/semaphore-protocol/benchmarks" isExternal>
